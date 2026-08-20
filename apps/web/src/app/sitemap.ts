a0ba0015f3cf63a571f66737c86fffee4655114f
Aquio Fulgencio <151652426+akiakyo@users.noreply.github.com>
2026-08-21 00:47:10 +0800

    fix: sitemap

--- apps/web/src/app/sitemap.ts
+++ apps/web/src/app/sitemap.ts
@@ -1,10 +1,56 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  const baseUrl = "https://sintachat.com";
+
   return [
     {
-      url: "https://sintachat.com",
+      url: baseUrl,
       lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+
+    {
+      url: `${baseUrl}/faq`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+
+    {
+      url: `${baseUrl}/safety`,
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.8,
+    },
+
+    {
+      url: `${baseUrl}/privacy`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+
+    {
+      url: `${baseUrl}/terms`,
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
+
+    {
+      url: `${baseUrl}/wall`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7,
+    },
+
+    {
+      url: `${baseUrl}/finding`,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 0.7,
     },
   ];
 }
